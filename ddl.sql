51b5a64905710348e0b0aa38c75493cf4aa584a6
fudgeythewhaler <62308952+fudgeythewhaler@users.noreply.github.com>
2020-03-18 11:29:36 -0400

    Update ddl.sql
    added FactDisease

--- ddl.sql
+++ ddl.sql
@@ -41,7 +41,32 @@ CREATE TABLE [Covid].[DimGeoLocation](
 )WITH (STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
-
+CREATE TABLE [Covid].[FactDisease](
+	[DimDateKey] [int] NOT NULL,
+	[DimGeoLocationKey] [int] NOT NULL,
+	[Disease] [varchar](25) NOT NULL,
+	[Confirmed] [int] NOT NULL,
+	[Deaths] [int] NOT NULL,
+	[Recovered] [int] NOT NULL,
+	[NumberOfDaysFirstOutbreak] [int] NOT NULL,
+ CONSTRAINT [PK_Covid_FactDisease] PRIMARY KEY CLUSTERED 
+(
+	[DimDateKey] ASC,
+	[DimGeoLocationKey] ASC,
+	[Disease] ASC
+)WITH (STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+ALTER TABLE [Covid].[FactDisease]  WITH CHECK ADD  CONSTRAINT [FK_Covid_FactDisease_DimDateKey] FOREIGN KEY([DimDateKey])
+REFERENCES [Covid].[DimDate] ([DimDateKey])
+GO
+ALTER TABLE [Covid].[FactDisease] CHECK CONSTRAINT [FK_Covid_FactDisease_DimDateKey]
+GO
+ALTER TABLE [Covid].[FactDisease]  WITH CHECK ADD  CONSTRAINT [FK_Covid_FactDisease_DimGeoLocationKey] FOREIGN KEY([DimGeoLocationKey])
+REFERENCES [Covid].[DimGeoLocation] ([DimGeoLocationKey])
+GO
+ALTER TABLE [Covid].[FactDisease] CHECK CONSTRAINT [FK_Covid_FactDisease_DimGeoLocationKey]
+GO
 ALTER TABLE [Covid].[FactCovid]  WITH CHECK ADD  CONSTRAINT [FK_Covid_FactCovid_DimDateKey] FOREIGN KEY([DimDateKey])
 REFERENCES [Covid].[DimDate] ([DimDateKey])
 GO
